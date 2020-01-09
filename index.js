3224d27f9c52e063997a126de4924800e1a9dab8
Mikey Mann <mikey.mannx3@gmail.com>
2020-01-08 19:18:16 -0800

    Fixed DOM render and style

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//Retrieves the dog image from the dog API endpoint
+
 function getDogImage(dogInput) {
   fetch(`https://dog.ceo/api/breeds/image/random/${dogInput}`)
     .then(response => response.json())
@@ -8,10 +10,19 @@ function getDogImage(dogInput) {
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+//This function handles the displaying of the actual amount of dogs chosen by the user.
+//We first empy the section and then display the images each time the button is pressed.
 
 function displayResults(responseJson) {
     console.log(responseJson);
+    $('.js-search-results').empty();
+    for (let i = 0; i < responseJson.message.length; i++) {
+      $('.js-search-results').append(`<img src="${responseJson.message[i]}">`);
+    }
   }
+
+
+//Watches for the submit button interaction
 
 function watchForm() {
   $('form').submit(event => {
@@ -20,6 +31,8 @@ function watchForm() {
     getDogImage(dogInput);
   });
 }
+
+// Initilize dog app
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
